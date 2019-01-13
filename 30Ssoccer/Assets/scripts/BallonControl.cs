50b45876ff8ce3d3a20674532b0bba31587bb583
Florian <flolevy33@gmail.com>
2019-01-13 01:02:05 -0500

    ajout delai quand but

--- 30Ssoccer/Assets/scripts/BallonControl.cs
+++ 30Ssoccer/Assets/scripts/BallonControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
 
 public class BallonControl : MonoBehaviour
 {
@@ -9,28 +10,44 @@ public class BallonControl : MonoBehaviour
     public int scoreValue = 1;
 
     private PlayerControl playerControl;
+    private GameObject J1;
+    private GameObject J2;
+
 
     void Start()
     {
+        J1 = GameObject.FindWithTag("But1");
+
+        J2 = GameObject.FindWithTag("But2");
     }
 
     void OnTriggerEnter(Collider collision)
     {
 
         Debug.Log("COLLISION");
+        GameObject playerControlObject = GameObject.FindWithTag(collision.gameObject.name);
+        playerControl = playerControlObject.GetComponent<PlayerControl>();
+
         if (collision.gameObject.name == "But2")
         {
-            GameObject playerControlObject = GameObject.FindWithTag("But2");
-            playerControl = playerControlObject.GetComponent<PlayerControl>();
             playerControl.addScore(scoreValue);
 
         }
         else if (collision.gameObject.name == "But1")
         {
-            GameObject playerControlObject = GameObject.FindWithTag("But1");
-            playerControl = playerControlObject.GetComponent<PlayerControl>();
             playerControl.addScore(scoreValue);
 
         }
+        StartCoroutine(replaceGame());
+    }
+
+    IEnumerator replaceGame()
+    {
+
+        yield return new WaitForSeconds(3);
+        J1.transform.position= new Vector3(-10, J1.transform.position.y);
+        J2.transform.position = new Vector3(10, J2.transform.position.y);
+
+
     }
 }
