e90a686474905166d2dfb294ad50abbccfa573f3
Florian <flolevy33@gmail.com>
2019-01-12 21:28:58 -0500

    controles du joueur

---
+++ 30Ssoccer/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+
+{
+    private Vector3 movementVector;
+
+    public string horizontalCtrl = "Horizontal_P1";
+    public string verticalCtrl = "Vertical_P1";
+
+    private CharacterController characterController;
+
+    public float movementSpeed = 15;
+
+    private float gravity = 40;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxis(horizontalCtrl) * movementSpeed;
+        movementVector.z = Input.GetAxis(verticalCtrl) * movementSpeed;
+
+
+        movementVector.y -= gravity * Time.deltaTime;
+
+        characterController.Move(movementVector * Time.deltaTime);
+
+    }
+}
